d502aa359b3d95d9aad31a20e8f0d97281003db5
AndyDingo <nwcreations@gmail.com>
2016-09-19 23:55:42 +0800

    Build number increment, forgot to add to last build

--- pfsTelegramBot/Properties/AssemblyInfo.cs
+++ pfsTelegramBot/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@
  * Created by: Microsoft Visual Studio 2015.
  * User      : AndyDingoWolf
  * -- VERSION --
- * Version   : 1.0.0.89
+ * Version   : 1.0.0.90
  */
 
 using System.Reflection;
@@ -45,9 +45,9 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.89")]
-[assembly: AssemblyFileVersion("1.0.0.89")]
-[assembly: AssemblyInformationalVersion("1.0.0.89")]
+[assembly: AssemblyVersion("1.0.0.90")]
+[assembly: AssemblyFileVersion("1.0.0.90")]
+[assembly: AssemblyInformationalVersion("1.0.0.90")]
 [assembly: NeutralResourcesLanguageAttribute("en-AU")]
 
 //[assembly: PermissionSetAttribute(SecurityAction.RequestMinimum, Name = "FullTrust")]
