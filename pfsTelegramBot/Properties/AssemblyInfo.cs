18695912b4a435c24e46f95e41a437961e03bed0
AndyDingo <nwcreations@gmail.com>
2017-03-02 19:04:47 +0800

    Revert "fd"
    This reverts commit c219d7c9d6563628ba851f2a3d708290fa579ef8.

--- pfsTelegramBot/Properties/AssemblyInfo.cs
+++ pfsTelegramBot/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@
  * Created by: Microsoft Visual Studio 2015.
  * User      : AndyDingoWolf
  * -- VERSION --
- * Version   : 1.0.0.118
+ * Version   : 1.0.0.117
  */
 
 using System.Reflection;
@@ -45,9 +45,9 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.118")]
-[assembly: AssemblyFileVersion("1.0.0.118")]
-[assembly: AssemblyInformationalVersion("1.0.0.118")]
+[assembly: AssemblyVersion("1.0.0.117")]
+[assembly: AssemblyFileVersion("1.0.0.117")]
+[assembly: AssemblyInformationalVersion("1.0.0.117")]
 [assembly: NeutralResourcesLanguageAttribute("en-AU")]
 
 //[assembly: PermissionSetAttribute(SecurityAction.RequestMinimum, Name = "FullTrust")]
