d2a86373cc87d961f0c9183ed8edd68de447285b
Jessica E <downs_morganaj@outlook.com.au>
2019-12-10 14:03:41 +0800

    mew

--- pfsTelegramBot/cDiceBag.cs
+++ pfsTelegramBot/cDiceBag.cs
@@ -1,5 +1,5 @@
 ﻿/* 
- * All contents copyright 2016-2018, Andy Dingo and Contributors
+ * All contents copyright 2016-2019, Andy Dingo and Contributors
  * All rights reserved.  YOU MAY NOT REMOVE THIS NOTICE.
  * Please read docs/gpl.txt for licensing information.
  * ---------------------------------------------------------------
